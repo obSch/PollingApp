01c228b9278b7487fcaba2c0fb449fd3cd6341c4
obSch <obmakno@gmail.com>
2021-01-24 02:40:51 +0200

    Implemented Spring Security.

---
+++ pollingapp/src/main/java/com/obmaikano/pollingapp/security/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.obmaikano.pollingapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.obmaikano.pollingapp.models.User;
+import com.obmaikano.pollingapp.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+		
+		// let people login with either username or email
+		User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+				.orElseThrow(() -> 
+						new UsernameNotFoundException("User not found with username or email : " +usernameOrEmail)
+		);
+		
+		return UserPrincipal.create(user);
+	}
+	
+	// method used by JWTAuthenticationFilter
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		User user = userRepository.findById(id).orElseThrow(
+				() -> new UsernameNotFoundException("User not found with id: " +id)
+		);
+		
+		return UserPrincipal.create(user);
+	}
+
+}
